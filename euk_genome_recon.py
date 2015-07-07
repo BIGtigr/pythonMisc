857626747fb5c23a2dfd84878c9fa778817a3c43
aemann01 <allison.e.mann@gmail.com>
2015-07-07 16:13:22 -0500

    Added filter function (incomplete)

--- euk_genome_recon.py
+++ euk_genome_recon.py
@@ -1,6 +1,6 @@
 #Skeleton version
 import sys, os, subprocess
-#import optparse
+from Bio import SeqIO
 
 def main():
 	if '-paired' in sys.argv:
@@ -10,9 +10,10 @@ def main():
 
 def bowtie2_paired():
 	print "Working Directory: ", os.getcwd()
+	print "\n"
 	if __name__ == '__main__':
 		if len(sys.argv) < 7:
-			print "Unacceptable number of arguments passed to script"
+			sys.exit("Error: Unacceptable number of arguments passed to script")
 		else:
 			pass
 
@@ -22,31 +23,70 @@ def bowtie2_paired():
 	SE = sys.argv[4]
 	sam_file = sys.argv[5] 
 	stats_file = sys.argv[6]
-	command = ('bowtie2 --no-unal -x' + ' ' + ref + ' ' + '-1' + ' ' + R1 + ' ' + '-2' + ' ' + R2 + ' ' + '-U' + SE)
-	print "\n"
+
+	command = ('bowtie2 --no-unal -x' + ' ' + ref + ' ' + '-1' + ' ' + R1 + ' ' + '-2' + ' ' + R2 + ' ' + '-U'+ ' ' + SE)
 	print "Running paired end bowtie2 mapping: ", command	
 	
 	with open('bowtie2_out.sam', 'w') as sam, open('bowtie2_out.stats', 'w') as stats:
 		subprocess.call(command, shell=True, stdout=sam, stderr=stats)
+	sam.close()
+	stats.close()
 
-main()
+	filter_hits()
 
-#def bowtie2_single(reference, reads):
-#	ref = reference
-#	R1 = reads
-#	command = pass
-#	subprocess.call(command)
 
-#def filter_hits():
-#	pass
+def bowtie2_single():
+	print "Working Directory: ", os.getcwd()
+	print "\n"
+	if __name__ == '__main__':
+		if len(sys.argv) < 5:
+			sys.exit("Error: Unacceptable number of arguments passed to script")
+		else:
+			pass	
 
-#def blast_align():
-#	pass
+	ref = sys.argv[1]
+	read = sys.argv[2]
+	sam_file = sys.argv[3]
+	stats_file = sys.argv[4]
 
-#def gen_consensus():
-#	pass
+	command = ('bowtie2 --no-unal -x' + ' ' + ref + ' ' + '-U' + ' ' + read)
+	print "Running single end bowtie2 mapping: ", command
 
-#def per_base_coverage():
-#	pass
+	with open('bowtie2_out.sam', 'w') as sam, open('bowtie2_out.stats', 'w') as stats:
+		subprocess.call(command, shell=True, stdout=sam, stderr=stats)
+	sam.close()
+	stats.close()
 
-#graphics options
+	filter_hits()
+
+def filter_hits():
+	with open('bowtie2_out.sam') as f:
+		contents = f.read().splitlines(True)
+		
+		#get just hit IDs (first column of blast output)
+		hits = ''
+		for line in (line for line in contents if not line.startswith('@')):
+			hits += line
+			part_hits = hits.partition("\t")[0]
+			split_hits = part_hits.splitlines()
+		print "Number of sequences that mapped to the reference: ", len(split_hits)
+
+		#filter sequences corresponding to hit IDs from original fastq files
+		#NEED TO TEST THIS CHUNK
+		fw_records = (i for i in SeqIO.parse(sys.argv[2], "fastq") if i.id in split_hits)
+		rw_records = (i for i in SeqIO.parse(sys.argv[3], "fastq") if i.id in split_hits) 
+		se_records = (i for i in SeqIO.parse(sys.argv[4], "fastq") if i.id in split_hits) 		
+	
+
+filter_hits()
+
+def blast_align():
+	pass
+
+def get_consensus():
+	pass
+
+def per_base_coverage():
+	pass
+
+#main()
