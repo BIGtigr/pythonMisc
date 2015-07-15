71f698d73870905d9b77bae9904dee0c565d3fdc
aemann01 <allison.e.mann@gmail.com>
2015-07-15 17:29:23 -0500

    clean up

--- fasta_trim_length.py
+++ fasta_trim_length.py
@@ -2,25 +2,32 @@ from Bio import SeqIO
 import sys
 import pylab
 import numpy as np
+import matplotlib.pyplot as plt
 
-sizes = [len(rec) for rec in SeqIO.parse("test.fasta", "fasta")]
-print "Number of sequences: ", len(sizes), "\n", "Minimum length: ",  min(sizes), "\n", "Maximum length: ", max(sizes)
-print "Median length: ", np.median(sizes)
-pylab.hist(sizes, bins=20)
-pylab.show()
+def fasta_trim_length():
+	infasta = sys.argv[1]
 
-trim_metric = np.median(sizes)
-print trim_metric
+	#get size statistics and generate length histogram
+	sizes = [len(rec) for rec in SeqIO.parse(infasta, "fasta")]
+	print "Number of sequences: ", len(sizes), "\n", "Minimum length: ",  min(sizes), "\n", "Maximum length: ", max(sizes)
+	print "Median length: ", np.median(sizes)
+	plt.hist(sizes, bins=25)
+	plt.savefig('length_hist.pdf')
+	plt.show()
 
-"""Trim fasta file based on length of sequence."""
-kept_seqs = []
+	#trim fasta based on median size
+	trim_metric = np.median(sizes)
+	print "Keeping sequences that are more than or equal to: ", trim_metric
+	kept_seqs = []
 
-for i in SeqIO.parse(open("test.fasta", "rU"), "fasta"):
-	if len(i.seq) >= trim_metric:
-		kept_seqs.append(i)
+	for i in SeqIO.parse(open(infasta, "rU"), "fasta"):
+		if len(i.seq) >= trim_metric:
+			kept_seqs.append(i)
 
-print "Kept %i sequences" % len(kept_seqs)
+	print "Kept %i sequences" % len(kept_seqs)
 	
-output_fasta = open("len_trimmed.fasta", "w")
-SeqIO.write(kept_seqs, output_fasta, "fasta")
-output_fasta.close()
+	output_fasta = open("len_trimmed.fasta", "w")
+	SeqIO.write(kept_seqs, output_fasta, "fasta")
+	output_fasta.close()
+
+fasta_trim_length()
