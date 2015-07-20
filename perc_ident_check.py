a3e3b59729a6232fcaf5fe2d2e155a8f067ec52f
aemann01 <allison.e.mann@gmail.com>
2015-07-20 12:03:30 -0500

    Changed to more compact version, there are some problems -- if the top percent identity of one sequence is the same as second or third top hit for a different sequence this will still get pulled.

--- perc_ident_check.py
+++ perc_ident_check.py
@@ -1,50 +1,28 @@
 import numpy as np
+import sys
+import pandas as pd
+from pandas import Series, DataFrame
 
-def perc_ident_check():
-	"""Checks tablular output from blast. Keeps all hits that have the same sequence ID and equivalent percent identity. Sequence ID must be first column, percent identity third column
-	Output saved as tab separated file: sorted_blast_by_perc_id.txt
-	"""
-	arr = np.genfromtxt('test_array.txt', delimiter="\t", dtype=None)
-	i = 0
-	j = 1
-	row_length = arr.shape[0]
-	collection = []
-
-	for row in range(1, row_length):
-		#check to see if indexed value exists, if not break loop		
-		try:
-			t=arr[j]
-		except IndexError: 
-			print "end of list"
-			break
-		#if sequence ids do not match skip
-		if arr[i][0] != arr[j][0]:
-			print "Sequence IDs do not match"
-			i += 1
-			j += 1
-			continue
-		#if sequence id's match and percent identity is the same 
-		elif arr[i][2] == arr[j][2]:
-			print "Rows match exactly"
-			#both the first and second match append to collection
-			collection.append(arr[i])
-			if arr[j] not in collection:
-				print "Second row matches and is not in collection"
-				collection.append(arr[j])			
-			i += 2
-			j += 2
-			continue			
-		elif arr[i][2] > arr[j][2] and arr[i][0] not in collection:
-			print "First row is greater than second"
-			collection.append(arr[i])
-			i += 2
-			j += 2
-			continue
-		else:
-			print "something is wrong"
-			break
-	print "Final collection contents: \n", collection
+def perc_id_check():
+	blast_in = sys.argv[1]
+	arr = np.genfromtxt(blast_in, delimiter='\t', dtype=None)
+	df = DataFrame(arr)
+	keys = dict(df.groupby(['f0'], sort=True)['f2'].max())
+	for key, value in keys.iteritems():
+		print "Top hit:", key, value
+	collection = df[(df['f0'].isin(keys.keys())) & (df['f2'].isin(keys.values()))]
+	print "Final collection contents: \n", df[(df['f0'].isin(keys.keys())) & (df['f2'].isin(keys.values()))]	
+	np.savetxt('sorted_blast_out.txt', collection, delimiter='\t', fmt='%s')
 	
-	np.savetxt('sorted_blast_by_perc_id.txt', collection, delimiter="\t", fmt="%s")
 
-perc_ident_check()
+perc_id_check()
+
+#if df[0][0] == items[0][0] 
+
+
+
+#keys = np.unique(sorted_df['f0'])
+#print keys
+
+#grouped = sorted_df.groupby(keys)
+#print grouped
