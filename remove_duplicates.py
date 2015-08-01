813e328e88d7f038de14f663d09631feff9e4862
aemann <allison.e.mann@gmail.com>
2015-07-31 19:03:55 -0500

    Working on remove optical duplicate script. Checking for same sample ID failing so far

---
+++ remove_duplicates.py
@@ -0,0 +1,134 @@
+#!/usr/local/bin/python
+
+import pandas as pd
+from pandas import DataFrame
+import sys
+import numpy as np
+
+def find_pcr_opt_dups(dups):
+	#initalize empty lists
+	optDups = []
+	pcrDups = []
+	possibleOptNames = []
+	
+	#add bin groups and group dataframe
+	dupsBin = dups.assign(bin_group = dups['titleCig'] + dups['ref'])
+	dupGroup = dupsBin.groupby(['bin_group', 'start'], as_index=False)
+
+	#if there are duplicates but all from same sample == PCR duplicate, else add group name to list
+	###THIS IS FAILING!!##
+	for name, group in dupGroup:
+		grouped_samples = dupGroup.get_group(name)['sampleID']
+		if len(grouped_samples.unique()) == 1:
+			pcrDups.append(dupGroup.get_group(name)['record'])
+		else:
+			possibleOptNames.append(name)
+	print possibleOptNames
+
+	nit = 0
+	for i in range(0, len(possibleOptNames)):
+		xvals = dupGroup.get_group(possibleOptNames[nit])['x'].reset_index()
+		yvals = dupGroup.get_group(possibleOptNames[nit])['y'].reset_index()
+		records = dupGroup.get_group(possibleOptNames[nit])['record'].reset_index()
+		print "collecting data from group", possibleOptNames[nit]
+		nit += 1
+		fin = 0
+		sin = 1
+
+		for k in range(0, len(xvals)):
+			xi = xvals['x'][fin]
+			xj = xvals['x'][sin]
+			yi = yvals['y'][fin]
+			yj = yvals['y'][sin]
+			if abs(int(xi)-int(xj)) > 100:
+				pcrDups.append(records['record'][fin])
+				fin += 1
+				sin += 1
+			elif abs(int(yi)-int(yj)) <= 100:
+				if records['record'][fin] not in optDups:			
+					optDups.append(records['record'][fin])
+				if records['record'][sin] not in optDups:			
+					optDups.append(records['record'][sin])
+				fin += 1
+				sin += 1
+				break
+			else:
+				print "other"
+
+	print pcrDups
+	print optDups
+
+	with open("pcr_duplicates.txt", "w") as pcrOut:
+		for record in pcrDups:
+			print >> pcrOut, record
+	pcrOut.close()
+	with open("optical_duplicates.txt", "w") as optOut:
+		for record in optDups:
+			print >> optOut, record
+	optOut.close()
+
+
+def read_sam_get_nondups(inputfile):
+	"""Loads and extracts data from sorted sam file
+	"""
+	data = []
+	with open(inputfile, "r") as f, open("read_info.txt", "w") as outfile:
+		for line in f:
+			try:
+				record = line.split()
+				t=record[2]
+			except IndexError:
+				break
+	
+			if line.startswith("@"):
+				pass
+			else:
+				record = line.split()
+				ref = record[2]
+				start = record[3]
+				cigar = record[5]
+				read = record[0].split(":")
+				sampleID = read[0]
+				title = read[4]
+				x = read[5]
+				y = read[6]
+				titleCig = title + "_" + cigar
+				data.append([titleCig, ref, start, x, y, sampleID, line])
+		dfSam = DataFrame(data)
+		dfSam.to_csv(outfile, sep="\t", index=False, header=False)
+		outfile.close()
+
+	nondupsOut = "nonduplicates.txt"
+	dupsOut = "duplicates.txt"
+
+	dfSam.columns = ['titleCig', 'ref', 'start', 'x', 'y', 'sampleID', 'record']
+	dfSam['count'] = dfSam.groupby('start')['start'].transform('count')
+
+	nondups = dfSam[dfSam['count'] == 1]
+	nondups.record.to_csv(nondupsOut, index=False, header=False)
+	dups = dfSam[dfSam['count'] > 1]
+	dups.to_csv(dupsOut, sep="\t", index=False, header=False)
+	find_pcr_opt_dups(dups)
+
+
+def main():
+	inputfile = sys.argv[1]
+	read_sam_get_nondups(inputfile)
+
+main()
+
+	
+		
+
+	
+		
+
+
+
+
+		
+	
+
+
+		
+
