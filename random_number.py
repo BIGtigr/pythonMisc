51b1984a59c20cb9da6aa4feddc4fb2aafd7bbeb
aemann <allison.e.mann@gmail.com>
2015-08-27 15:29:20 -0500

    cleaned up, added number of draws option

--- random_number.py
+++ random_number.py
@@ -1,8 +1,11 @@
 import random
 import sys
 
+"""Random number generator without replacement. Useage: python random_number.py <upper limit range> <number of draws>. Outputs to text file random_numbers.txt
+"""
+
 with open("random_numbers.txt", "w") as f:
-	list = random.sample(range(1,int(sys.argv[1])), 5000000)
+	list = random.sample(range(1,int(sys.argv[1])), int(sys.argv[2]))
 	for item in list:
 		print >> f, item
 
