8b661e919f646cf81df78edc50141f809551c8a0
aemann <allison.e.mann@gmail.com>
2015-10-21 13:38:12 -0500

    Updated blast reference check program, now should have proper output

--- random_number.py
+++ random_number.py
@@ -1,13 +1,21 @@
 import random
 import sys
+import numpy as np
 
-"""Random number generator without replacement. Useage: python random_number.py <upper limit range> <number of draws>. Outputs to text file random_numbers.txt
+"""Pseudo-random number generator without replacement. Useage: python random_number.py <upper limit range> <number of draws>. Outputs to text file random_numbers.txt
 """
 
 with open("random_numbers.txt", "w") as f:
-	list = random.sample(xrange(1,int(sys.argv[1])), int(sys.argv[2]))
+	list = random.sample(xrange(1,int(sys.argv[1])+1), int(sys.argv[2]))
 	for item in list:
 		print >> f, item
 
+	f.close()
 
+#Numpy solution
+with open("randomNumber.txt", "w") as f1:
+	nums = np.random.randint(1, high=int(sys.argv[1])+1, size=int(sys.argv[2])
+	for item in nums:
+		print >> f1, item 	
 
+	f1.close()
--- ref_blast_check.py
+++ ref_blast_check.py
@@ -1,9 +1,7 @@
 #!/usr/local/bin/python
 
-import pandas as pd
 from pandas import DataFrame
 import sys
-import csv
 
 """This program reads in a list of GI accession numbers from NCBI and a tab separated blast output file where the query sequence ID is the first column, reference ID in second, and percent identity is fourth from the last column. Returns a list of query IDs that are either the only match or represent the top matches for some taxa or group of taxa as represented in the GI query list. Usage: python ref_blast_check.py gi_accession.list blast_output.txt
 """
@@ -17,6 +15,7 @@ with open(sys.argv[1], "r") as f:
 
 blast_dat = []
 with open(sys.argv[2], "r") as f:
+	print "File: %s" % sys.argv[2]
 	for line in f:
 		if not line.strip():
 			continue
@@ -27,7 +26,10 @@ with open(sys.argv[2], "r") as f:
 			except IndexError:
 				break
 		record = line.split()
-		ref = record[1].split("|")
+		if record[1].startswith("N"):
+			ref = record[1]
+		else:
+			ref = record[1].split("|")
 		seqID = record[0]
 		percID = record[-4]
 		giNum = ref[1]
@@ -35,13 +37,14 @@ with open(sys.argv[2], "r") as f:
 	blastData = DataFrame(blast_dat)
 	blastData.columns = ['seqID', 'giNum', 'percID']
 
-
 #get singleton list
 blastData['counts'] = blastData.groupby('seqID')['seqID'].transform('count')
 singletons = blastData.loc[blastData['counts'] == 1]
 
 singleHits = singletons[singletons['giNum'].isin(gi_list)]
+cleaned = singleHits.to_string(columns=['seqID', 'giNum'], index=False, header=False, justify='left')
 
+print cleaned
 #get double list
 doubles = blastData.loc[blastData['counts'] >=2]
 
@@ -56,14 +59,33 @@ hit_list = []
 #run thorough duplicate list, sort rows by percent id, if top two values are in gi list, add to hit list
 init = 0
 for i in range(0, len(seqID_list)):
-	groupedHits = groups.get_group(seqID_list[init]).sort('percID').reset_index()
+	groupedHits = groups.get_group(seqID_list[init]).sort('percID', ascending=False).reset_index()
 	first = groupedHits['giNum'][0]
 	second = groupedHits['giNum'][1]
-	if first and second in gi_list:
-		hit_list.append(groupedHits['seqID'][0])
-        init += 1
+
+	percID_diff = float(groupedHits['percID'][0]) - float(groupedHits['percID'][1])
+	#need to get around problems with natural ordering of floats with pandas, check if last values are 100% identity
+	if groupedHits['percID'].iloc[-1] and groupedHits['percID'].iloc[-2] == '100.00':
+		#print "Top values are both 100% identity"
+		if groupedHits['giNum'].iloc[-1] and groupedHits['giNum'].iloc[-2] in gi_list:
+			print "%s \t %s \t %s" % (groupedHits['seqID'].iloc[-1], groupedHits['giNum'].iloc[-1], groupedHits['giNum'].iloc[-2])
+			hit_list.append(groupedHits['seqID'].iloc[-1])
+		init += 1
+	else:
+		#if the difference between the top and second top percent ID is more than or equal to 3
+		if percID_diff >= 5.00:
+			#print "difference between top percent ID is more than or equal to 2.00"
+			if first in gi_list:
+				print "%s \t %s" % (groupedHits['seqID'][0], first)
+				hit_list.append(groupedHits['seqID'][0])
+			init += 1
+		else:
+			if first and second in gi_list:
+				print "%s \t %s \t %s" % (groupedHits['seqID'][0], first, second)
+				hit_list.append(groupedHits['seqID'][0])
+	        	init += 1
 
 hit_list.append(singleHits['seqID'].tolist())
 
-for line in hit_list:
-	print "".join(str(item) for item in line)
+#for line in hit_list:
+#	print "".join(str(item) for item in line)
