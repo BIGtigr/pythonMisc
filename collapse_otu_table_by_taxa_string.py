2af23b4cfc72b36250e854b13b32f524057534ce
aemann <allison.e.mann@gmail.com>
2015-11-06 09:35:14 -0600

    Collapse otu table by matching taxa strings. Fixes problem brought on by formatting differences when merging otu tables

---
+++ collapse_otu_table_by_taxa_string.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python
+
+import pandas as pd
+from pandas import DataFrame
+import sys
+
+"""Reads in tab delimited OTU table and returns OTU table collapsed by taxa string (useful for duplicates brought on by formatting issues. OTU table must have no commented out lines). Usage: python collapse_otu_table_by_taxa_string.py input_otu_table.txt
+"""
+#need to first remove comment lines and symbols from otu table. can re add in later. 
+
+df = pd.read_csv(sys.argv[1], sep="\t")
+collapsed_df = df.groupby("OTU ID").sum()
+
+with open("collapsed_otu_table.txt", "w") as outfile:
+	collapsed_df.to_csv(outfile, sep="\t")
+
+outfile.close()
